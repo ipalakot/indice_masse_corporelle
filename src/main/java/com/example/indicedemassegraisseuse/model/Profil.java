ef7421bb521da4cd17987f8d6b8f8dd6282a2a69
ipalakot <ipalakot@gmail.com>
2019-04-01 17:51:25 +0200

    Construction du Modal / Profil

---
+++ src/main/java/com/example/indicedemassegraisseuse/model/Profil.java
@@ -0,0 +1,86 @@
+package com.example.indicedemassegraisseuse.model;
+
+public class Profil {
+
+
+    // Constantes
+    private static final Integer minFemme = 15; // Maigre si au dessous
+    private static final Integer maxFemme = 30; // Gros si au dessou
+    private static final Integer minHomme = 10; // Maigre si au dessous
+    private static final Integer maxHomme = 25; // Gros si au dessou
+
+    // Variable
+    private Integer poids;
+    private Integer taille;
+    private Integer age;
+    private Integer sexe;
+    private String message;
+    private Float img;
+
+    /**
+     * Constructeur
+     *
+     * @param poids
+     * @param taille
+     * @param age
+     * @param sexe
+     */
+    public Profil(Integer poids, Integer taille, Integer age, Integer sexe) {
+        this.poids = poids;
+        this.taille = taille;
+        this.age = age;
+        this.sexe = sexe;
+    }
+
+    public Integer getPoids() {
+        return poids;
+    }
+
+    public Integer getTaille() {
+        return taille;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getSexe() {
+        return sexe;
+    }
+
+    /**
+     *
+     */
+    private void calculImg(){
+
+        float tailleM = ((float)taille) / 100;
+        this.img = (float)((1.2*poids / (tailleM*tailleM))*(0.23*age)-(10.83*sexe) - 5.4);
+    }
+
+    /**
+     *
+     */
+    public void resulImg(){
+        Integer min;
+        Integer max;
+
+        if (sexe == 0) {//Le cas d'une Femme
+            min = minFemme;
+        max = maxFemme;
+
+        } else  { // / Le cas d'un homme
+            min = minHomme;
+            max= maxHomme;
+            }
+
+    // Gestion  des Messages
+        message = "normal";
+        if (img < min){
+            message ="Vous êtes tropmaiges !";
+        } else {
+            if (img > max) {
+                message = "Vous êtes bien Gros!";
+            }
+            }
+    }
+}
--- src/main/java/com/example/indicedemassegraisseuse/vue/MainActivity.java
+++ src/main/java/com/example/indicedemassegraisseuse/vue/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.indicedemassegraisseuse;
+package com.example.indicedemassegraisseuse.vue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.indicedemassegraisseuse.R;
 
 public class MainActivity extends AppCompatActivity {
 
